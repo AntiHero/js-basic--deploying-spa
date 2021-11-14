3304a83a7dea24444d8d383c88ed346dedd10f8f
Aleksandr Schemelev <prophreaker@gmail.com>
2021-11-14 23:20:48 +0300

    feat: add eventlistener to handle clicks on link tags

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
 import './styles/style.css';
+
+document.body.addEventListener('click', (e) => {
+  if ((<HTMLElement>e.target).tagName === 'A') {
+    (<HTMLElement>document.body.querySelector('p')).innerHTML =
+      window.location.href;
+  }
+});

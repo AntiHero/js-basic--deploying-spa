1a446d84d9114abe5beac7303de27d4d5d896c34
Aleksandr Schemelev <prophreaker@gmail.com>
2021-11-15 18:01:30 +0300

    feat: add render function

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,13 @@ import './styles/style.css';
 
 document.body.addEventListener('click', (e) => {
   if ((<HTMLElement>e.target).tagName === 'A') {
-    (<HTMLElement>document.body.querySelector('p')).innerHTML =
-      window.location.href;
+    render();
   }
 });
+
+function render() {
+  (<HTMLElement>document.body.querySelector('p')).innerHTML =
+    window.location.href;
+}
+
+render();
